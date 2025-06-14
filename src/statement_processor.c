6e37261cecf0cd8db55197d0cc04828562f417b8
AssemblerBossss <nazarovmisa39@mail.ru>
2025-06-14 13:52:03 +0300

    отдельная обработка запроса на вставку (функция prepare_insert,updated PrepareResult enum

--- src/statement_processor.c
+++ src/statement_processor.c
@@ -1,5 +1,30 @@
 #include "../include/statement_processor.h"
 
+PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement) {
+    statement->type = STATEMENT_INSERT;
+
+    char *keyword   = strtok (input_buffer->buffer," ,.-");
+    char *id_string = strtok (NULL, " ,.-");
+    char *username  = strtok (NULL, " ,.-");
+    char *email     = strtok (NULL ," ,.-");
+
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+
+    if (strlen(username) > COLUMN_USERNAME_SIZE || strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
 
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
