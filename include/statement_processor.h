d9af9d0e44d81d8ac1c7d5a1beaf236bb81fa4e5
AssemblerBossss <nazarovmisa39@mail.ru>
2025-06-14 22:48:42 +0300

    docs: добавление Doxygen-документации для statement_processor.h
    - Добавлены комментарии для всех функций процессора SQL-запросов
    - Указаны параметры, возвращаемые значения и предупреждения
    - Отмечены нереализованные методы (UPDATE/DELETE)

--- include/statement_processor.h
+++ include/statement_processor.h
@@ -7,13 +7,56 @@
 #include "table.h"
 #include "Utils.h"
 
+/**
+ * @brief Подготавливает INSERT-запрос из входного буфера
+ * @param[in] input_buffer Указатель на входной буфер с командой
+ * @param[in] statement Указатель на структуру Statement для заполнения
+ * @return Результат подготовки (PrepareResult)
+ *
+ * @note Разбирает строку формата: "insert id username email"
+ */
 PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement);
+
+/**
+ * @brief Определяет тип SQL-запроса (INSERT, SELECT...) и подготавливает Statement
+ * @param[in] input_buffer Указатель на входной буфер
+ * @param[in] statement Указатель на структуру Statement
+ * @return Результат подготовки (PrepareResult)
+ */
 PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
 
+/**
+ * @brief Выполняет INSERT-запрос
+ * @param[in] statement Указатель на подготовленный Statement
+ * @param[in] table Указатель на таблицу
+ * @return Результат выполнения (ExecuteResult)
+ *
+ * @note Добавляет строку в конец таблицы
+ * @warning Проверяет переполнение таблицы
+ */
 ExecuteResult execute_insert(Statement *statement, Table *table);
+
+/**
+ * @brief Выполняет SELECT-запрос
+ * @param[in] statement Указатель на подготовленный Statement
+ * @param[in] table Указатель на таблицу
+ * @return Результат выполнения (ExecuteResult)
+ *
+ * @note Выводит все строки таблицы в формате (id, username, email)
+ */
 ExecuteResult execute_select(Statement *statement, Table *table);
 ExecuteResult execute_update(Statement *statement, Table *table);
 ExecuteResult execute_delete(Statement *statement, Table *table);
+
+/**
+ * @brief Выполняет SQL-запрос на основе типа Statement
+ * @param[in] statement Указатель на подготовленный Statement
+ * @param[in] table Указатель на таблицу
+ * @return Результат выполнения (ExecuteResult)
+ *
+ * @note Маршрутизирует выполнение на конкретные функции
+ * @warning Не реализованы UPDATE и DELETE (заглушки)
+ */
 ExecuteResult execute_statement(Statement *statement, Table *table);
 
 #endif // DATABASE_STATEMENT_PROCESSOR_H
